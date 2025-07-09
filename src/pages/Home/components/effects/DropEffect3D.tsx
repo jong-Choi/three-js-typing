dd3f76c2af43b46f8c7cb84f85b701d97012f2a9
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2025-07-10 06:32:04 +0900

    [chore] 불필요한 import 삭제

--- src/pages/Home/components/effects/DropEffect3D.tsx
+++ src/pages/Home/components/effects/DropEffect3D.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useMemo } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { FontLoader, TextGeometry, OrbitControls, Font } from "three-stdlib";
 import * as CANNON from "cannon-es";
